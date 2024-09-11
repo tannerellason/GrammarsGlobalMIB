8c2dd488ef233a32cb347dc3c97af9485274673a
tannerellason <121735435+tannerellason@users.noreply.github.com>
2024-09-11 13:12:04 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -8,6 +8,6 @@ public static class ModHelperData
 
     public const string Description = "Literally just makes the MIB have global range. Made for TheNeoGod";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "ImperfectGrammar"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "GrammarsGlobalMIB"; // TODO add your repo name here, also in the download url in README.md
 }
